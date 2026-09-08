f17593f11c9dce317149afb734722b6d8b771d3d
Victor Matveyshin <vic_m@ukr.net>
2026-09-08 22:17:05 +0300

    class-base-view

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
 ]
 
 app_name = "taxi"
