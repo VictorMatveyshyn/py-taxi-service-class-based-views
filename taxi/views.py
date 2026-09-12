06abd5da1f1ea2d2ea4d6625bbe78797bb5a2b56
Victor Matveyshin <vic_m@ukr.net>
2026-09-12 22:35:27 +0300

    point 5

--- taxi/views.py
+++ taxi/views.py
@@ -15,23 +15,28 @@ def index(request):
 
     return render(request, "taxi/index.html", context=context)
 
+
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
     queryset = Manufacturer.objects.all().order_by("name")
     paginate_by = 5
+
 
 class CarListView(generic.ListView):
     model = Car
     queryset = Car.objects.select_related("manufacturer")
     paginate_by = 5
 
+
 class CarDetailView(generic.DetailView):
     model = Car
+
 
 class DriverListView(generic.ListView):
     model = Driver
     queryset = Driver.objects.all()
     paginate_by = 5
+
 
 class DriverDetailView(generic.DetailView):
     model = Driver
